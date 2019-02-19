e3d9472286f5d67cf34fcefe4fa4340ee3a7d35a
Daniel Solá <daniel.sola@tiendeo.com>
2019-02-19 12:18:08 +0100

    Replaced original error message

--- src/Error.js
+++ src/Error.js
@@ -2,11 +2,12 @@ const isError = arg => arg instanceof Error;
 class DetailError extends Error {
   constructor(...args) {
     const e = args.find(i => isError(i));
-    const message = args.reduce((acc, i) => `${acc}${isError(i) ? i.message : i}`, '');
+    const message = args.reduce((acc, i) => `${acc} ${isError(i) ? i.message : i}`, '')
     super(message);
     if (e) {
       Object.defineProperty(this, 'stack', {
         get: function() {
+          e.message = message;
           return e.stack;
         }
       });
